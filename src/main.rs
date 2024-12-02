5fcec931f1b6079d681c15adf5290d4dd36c158c
Emma Vedel <emmavedelpedersen@gmail.com>
2024-12-02 19:28:39 +0100

    add support for future half finished days

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         if let Ok(day) = input.trim().parse() {
             let (part1, part2) = days::run_solution(day);
 
-            println!("DAY {}:", day);
+            println!("  DAY {}", day);
             println!("PART 1: {}", part1);
-            println!("PART 2: {}", part2);
+
+            if part2 != 0 {
+                println!("PART 2: {}", part2);
+            }
 
             return Ok(());
         }
