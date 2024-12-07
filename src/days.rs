1e73455899886da2b9cddb7599dc8a16a4e2673c
Emma Vedel <emmavedelpedersen@gmail.com>
2024-12-07 13:03:41 +0100

    day 7

--- src/days.rs
+++ src/days.rs
@@ -4,15 +4,17 @@ mod day_03;
 mod day_04;
 mod day_05;
 mod day_06;
+mod day_07;
 
-pub fn run_solution(day: i32) -> (i32, i32) {
+pub fn run_solution(day: i32) -> (i64, i64) {
     match day {
-        1 => (day_01::part1(), day_01::part2()),
-        2 => (day_02::part1(), day_02::part2()),
-        3 => (day_03::part1(), day_03::part2()),
-        4 => (day_04::part1(), day_04::part2()),
-        5 => (day_05::part1(), day_05::part2()),
-        6 => (day_06::part1(), day_06::part2()),
+        1 => (day_01::part1() as i64, day_01::part2() as i64),
+        2 => (day_02::part1() as i64, day_02::part2() as i64),
+        3 => (day_03::part1() as i64, day_03::part2() as i64),
+        4 => (day_04::part1() as i64, day_04::part2() as i64),
+        5 => (day_05::part1() as i64, day_05::part2() as i64),
+        6 => (day_06::part1() as i64, day_06::part2() as i64),
+        7 => (day_07::part1(), day_07::part2()),
         _ => panic!(),
     }
 }
---
+++ src/days/day_07.rs
@@ -0,0 +1,78 @@
+fn get_input() -> Vec<(i64, Vec<i32>)> {
+    let input = std::fs::read_to_string("inputs/day_07.txt").unwrap();
+
+    input
+        .lines()
+        .map(|l| l.split(": "))
+        .map(|mut spliterator| {
+            let result = spliterator.next().unwrap().parse().unwrap();
+            let numbers = spliterator
+                .next()
+                .unwrap()
+                .split(' ')
+                .map(|s| s.parse().unwrap())
+                .collect();
+            (result, numbers)
+        })
+        .collect()
+}
+
+fn concat_numbers(a: i64, b: i64) -> i64 {
+    (a.to_string() + &b.to_string()).parse().unwrap()
+}
+
+fn calc_recursively(
+    expected_result: i64,
+    accumulator: i64,
+    numbers: &[i32],
+    use_third_operator: bool,
+) -> bool {
+    let cur_number = numbers[0] as i64;
+    if numbers.len() == 1 {
+        accumulator + cur_number == expected_result
+            || accumulator * cur_number == expected_result
+            || (use_third_operator && concat_numbers(accumulator, cur_number) == expected_result)
+    } else {
+        calc_recursively(
+            expected_result,
+            accumulator + cur_number,
+            &numbers[1..],
+            use_third_operator,
+        ) || calc_recursively(
+            expected_result,
+            accumulator * cur_number,
+            &numbers[1..],
+            use_third_operator,
+        ) || (use_third_operator
+            && calc_recursively(
+                expected_result,
+                concat_numbers(accumulator, cur_number),
+                &numbers[1..],
+                use_third_operator,
+            ))
+    }
+}
+
+pub fn part1() -> i64 {
+    let input = get_input();
+
+    input
+        .iter()
+        .filter(|(result, numbers)| {
+            calc_recursively(*result, numbers[0] as i64, &numbers[1..], false)
+        })
+        .map(|(result, _)| result)
+        .sum()
+}
+
+pub fn part2() -> i64 {
+    let input = get_input();
+
+    input
+        .iter()
+        .filter(|(result, numbers)| {
+            calc_recursively(*result, numbers[0] as i64, &numbers[1..], true)
+        })
+        .map(|(result, _)| result)
+        .sum()
+}
